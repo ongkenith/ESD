f4770750918ae3965c0918e1671f1b62b8bdeeaf
tinasjn <144519678+tinasin@users.noreply.github.com>
2025-03-12 14:52:06 +0800

    customer

--- customer.sql
+++ customer.sql
@@ -4,3 +4,11 @@ CREATE TABLE Customer (
     Email VARCHAR(255) UNIQUE NOT NULL,
     Mobile_No VARCHAR(8) NOT NULL
 );
+
+INSERT INTO Customer (Name, Email, Mobile_No) VALUES
+('Tina', 'tina@email.com', '91234567'),
+('Kenny', 'kenny@email.com', '92345678'),
+('Jordan', 'jordan@email.com', '93456789'),
+('Jasdev', 'jasdev@email.com', '94567890'),
+('Zhengjie', 'zhengjie@email.com', '95678901'),
+('Deshaun', 'deshaun@email.com', '96789012');
