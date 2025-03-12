a674b3debd2989d109b27e7b1e1b6c40cbd334e9
ongkenith <kenith.ong.2023@scis.smu.edu.sg>
2025-03-12 14:44:21 +0800

    Edited item and store sql

---
+++ item.sql
@@ -0,0 +1,10 @@
+DROP TABLE IF EXISTS `Item`;
+CREATE TABLE Item (
+  `item_id` int(11) NOT NULL PRIMARY AUTO_INCREMENT,
+  `name` varchar(255) NOT NULL DEFAULT 'NEW',
+  `store_id` int(11) NOT NULL,
+  `price` float(53) NOT NULL DEFAULT 0,
+  PRIMARY KEY (`item_id`)
+  FOREIGN KEY (`store_id`) REFERENCES Store(`store_id`)
+  
+) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8;
